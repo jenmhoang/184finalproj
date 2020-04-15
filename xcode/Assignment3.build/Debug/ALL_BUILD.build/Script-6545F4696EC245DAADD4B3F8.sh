342ca2e5dd5b09a2da8a507aa0c168370bb06f1f
Ethan Buttimer <ethan.buttimer@gmail.com>
2020-04-15 11:14:02 -0700

    xcode build

--- xcode/Assignment3.build/Debug/ALL_BUILD.build/Script-6545F4696EC245DAADD4B3F8.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/susanlin/Desktop/cs184/p3-2-pathtracer-sp20-susan-lin/xcode -f /Users/susanlin/Desktop/cs184/p3-2-pathtracer-sp20-susan-lin/xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION OBJDIR=$(basename "$OBJECT_FILE_DIR_normal") all
--- xcode/CGL/Assignment3.build/Debug/CGL.build/Objects-normal/x86_64/complex.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/susanlin/Desktop/cs184/p3-2-pathtracer-sp20-susan-lin/CGL/src/complex.cpp \
-  /Users/susanlin/Desktop/cs184/p3-2-pathtracer-sp20-susan-lin/CGL/include/CGL/complex.h \
-  /Users/susanlin/Desktop/cs184/p3-2-pathtracer-sp20-susan-lin/CGL/include/CGL/CGL.h \
-  /Users/susanlin/Desktop/cs184/p3-2-pathtracer-sp20-susan-lin/CGL/include/CGL/misc.h \
-  /Users/susanlin/Desktop/cs184/p3-2-pathtracer-sp20-susan-lin/CGL/include/CGL/vector2D.h
