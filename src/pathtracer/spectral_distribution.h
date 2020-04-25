be26c79c305827dd60b336ba45569bb72bec12c0
Susan Lin <susan.lin@berkeley.edu>
2020-04-25 11:48:15 -0700

    wip

---
+++ src/pathtracer/spectral_distribution.h
@@ -0,0 +1,37 @@
+//
+//  spectral_distribution.h
+//  Assignment3
+//
+//  Created by Ethan Buttimer on 4/22/20.
+//
+
+#ifndef spectral_distribution_h
+#define spectral_distribution_h
+
+#include "CGL.h"
+#include "vector3D.h"
+#include <vector>
+
+using std::vector;
+
+namespace CGL {
+    class SpectralDistribution {
+        public:
+            SpectralDistribution(float temp);
+        
+            //may want a destructor?
+            //~SpectralDistribution(){};
+        
+            Spectrum toRGB();
+            Vector3D toXYZ();
+        
+        private:
+            int intensity;
+            float T;
+            vector<float> rel_distribution;
+        
+            float atWavelength(float lambda);
+            Spectrum rgbFromXYZ(Spectrum xyz_color);
+    };
+}
+#endif /* spectral_distribution_h */
