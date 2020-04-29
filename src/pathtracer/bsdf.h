27b9a2106e061ac3649b34214e6ad1d79bc4f4bb
Ethan Buttimer <ethan.buttimer@gmail.com>
2020-04-28 20:13:23 -0700

    five dragon images, refl 0.9 (90%)

--- src/pathtracer/bsdf.h
+++ src/pathtracer/bsdf.h
@@ -292,7 +292,7 @@ public:
     }
     
     Spectrum get_emission() const {
-        SpectralDistribution blackbody = SpectralDistribution(1100);
+        SpectralDistribution blackbody = SpectralDistribution(1500);
         Spectrum emitted = blackbody.toRGB();
         //std::cout << emitted << std::endl;
         return (1. - reflectance) * emitted;
